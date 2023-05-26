ce22d960a1a2bfcb08a32c2c4b2f0de2ddad9565
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-05-26 20:55:20 +0600

    add Cell component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Cell from './components/Cell/Cell';
 
-function App() {
+interface ICell {
+  hasItem: boolean;
+  clicked: boolean;
+}
+
+const App = () => {
+  const defaultCells: ICell[] = [];
+
+  for (let i: number = 1; i <= 36; i++) {
+    defaultCells.push({hasItem: false, clicked: false});
+  }
+
+  const [cells, setCell] = useState<ICell[]>(defaultCells);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        cells.map((cell: ICell, index: number) => {
+          return <Cell key={index}/>
+        })
+      }
     </div>
   );
 }
---
+++ src/components/Cell/Cell.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Cell.css';
+
+const Cell: React.FC = () => {
+  return (
+    <div className="Cell"></div>
+  );
+};
+
+export default Cell;
